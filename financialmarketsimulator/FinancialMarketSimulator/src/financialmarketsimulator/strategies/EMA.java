58ddc59e12d0c0db1396d2948f65bd6d7090e3f9
Moeletji <mdsemenya@gmail.com>
2014-06-29 21:25:56 +0200

    Edited EMA class

--- financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/strategies/EMA.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/strategies/EMA.java
@@ -1,11 +1,11 @@
 package financialmarketsimulator.strategies;
 
 /**
- *@brief EMA(Exponential Moving Average) 
+ * @brief EMA(Exponential Moving Average)
  * @author Grape <cos301.mainproject.grape@gmail.com>
  */
-
 public class EMA {
+
     /**
      * Number of days in EMA
      */
@@ -18,17 +18,22 @@ public class EMA {
      * Todays closing price
      */
     private double todaysPrice;
-    
-    public EMA(double closingPrice, int numDays, double prevEMA)
-    {
+
+    public EMA() {
+
+    }
+
+    @SuppressWarnings("UnusedAssignment")
+    public double calculateEMA(double closingPrice, int numDays, double prevEMA) {
+        if ((numDays <= 0) || (closingPrice <= 0) || (prevEMA <= 0)) {
+            return 0.0;
+        }
+
         numOfDays = numDays;
         todaysPrice = closingPrice;
         EMAYesterday = prevEMA;
-    }
-    
-    public double calculateEMA()
-    {
-        double k = 2/(numOfDays +1);
-        return (todaysPrice * k * EMAYesterday * (1-k));
+
+        double k = 2 / (numOfDays + 1);
+        return (todaysPrice * k * EMAYesterday * (1 - k));
     }
 }
