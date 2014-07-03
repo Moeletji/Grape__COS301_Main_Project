62a458df3f40d210405f48de8ae937c1253a0095
Moeletji <mdsemenya@gmail.com>
2014-07-03 15:18:49 +0200

    Added functionality to MatchedMarketEntryAttemptUpdate

--- financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/marketData/MatchedMarketEntryAttemptUpdate.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/marketData/MatchedMarketEntryAttemptUpdate.java
@@ -1,5 +1,7 @@
 package financialmarketsimulator.marketData;
 
+import java.util.Vector;
+
 /**
  *
  * @author Grape <cos301.mainproject.grape@gmail.com>
@@ -7,4 +9,54 @@ package financialmarketsimulator.marketData;
  
 public class MatchedMarketEntryAttemptUpdate {
     
+    private Vector<MatchedMarketEntryAttempt> tradesData;
+    
+    public MatchedMarketEntryAttemptUpdate(Vector<MatchedMarketEntryAttempt> trades)
+    {
+        tradesData = trades;
+    }
+    
+    public double getFirstTradePrice()
+    {
+        return (tradesData == null)? 0.0:tradesData.firstElement().getPrice();
+    }
+    
+    public double getLastTradePrice()
+    {
+        return (tradesData == null)? 0.0:tradesData.lastElement().getPrice();
+    }
+    
+    public double getHighestTradePrice()
+    {
+        if (tradesData == null)
+            return 0.0;
+        
+        double high = tradesData.get(0).getPrice();
+        
+        for (int i=1; i < tradesData.size(); i++)
+        {
+            if (high < tradesData.get(i).getPrice())
+            {
+                high = tradesData.get(i).getPrice();
+            }
+        }
+        return high;
+    }
+    
+    public double getLowestTradePrice()
+    {
+        if (tradesData == null)
+            return 0.0;
+        
+        double low = tradesData.get(0).getPrice();
+        
+        for (int i=1; i < tradesData.size(); i++)
+        {
+            if (low > tradesData.get(i).getPrice())
+            {
+                low = tradesData.get(i).getPrice();
+            }
+        }
+        return low;
+    }
 }
