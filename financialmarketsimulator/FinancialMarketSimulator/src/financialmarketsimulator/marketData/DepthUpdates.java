ad171285faa855bcc4b190e3672e94479536a641
danmakk <danmakk@gmail.com>
2014-07-04 13:42:39 +0200

    Update QuoteUpdates, StockManager, MarketManager

--- financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/marketData/DepthUpdates.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/marketData/DepthUpdates.java
@@ -1,10 +1,124 @@
 package financialmarketsimulator.marketData;
 
+import financialmarketsimulator.market.MarketEntryAttempt;
+import java.util.Date;
+
 /**
  *
  * @author Grape <cos301.mainproject.grape@gmail.com>
  */
+public final class DepthUpdates {
 
-public class DepthUpdates {
+    public static enum UpdateType {
+
+        NEW, UPDATE, ENTER, DELETE
+    }
+
+    private String stockName;
+
+    private Date time;
+
+    private String orderID;
+
+    private int numberOfShares;
+
+    private String updateType;
+
+    public DepthUpdates() {
+        time = new Date();
+    }
     
+    public DepthUpdates(String stockName, String orderID, int numberOfShares, String upDate) {
+        this();
+
+        this.setStockName(stockName);
+        this.setOrderID(orderID);
+        this.setNumberOfShares(numberOfShares);
+        this.setUpdateType(upDate);
+    }
+
+    public DepthUpdates(String stockName, String orderID, int numberOfShares, DepthUpdates.UpdateType upDate) {
+        this();
+
+        this.setStockName(stockName);
+        this.setOrderID(orderID);
+        this.setNumberOfShares(numberOfShares);
+        switch (upDate) {
+            case NEW:
+                this.setUpdateType("NEW");
+                break;
+            case DELETE:
+                this.setUpdateType("DELETE");
+                break;
+            case ENTER:
+                this.setUpdateType("ENTER");
+                break;
+            case UPDATE:
+                this.setUpdateType("UPDATE");
+                break;
+        }
+    }
+
+    public DepthUpdates(MarketEntryAttempt order, DepthUpdates.UpdateType upDate) {
+        this();
+
+        this.setStockName(order.getParticipantName());
+        this.setOrderID(order.getOrderID());
+        this.setNumberOfShares(order.getNumOfShares());
+        switch (upDate) {
+            case NEW:
+                this.setUpdateType("NEW");
+                break;
+            case DELETE:
+                this.setUpdateType("DELETE");
+                break;
+            case ENTER:
+                this.setUpdateType("ENTER");
+                break;
+            case UPDATE:
+                this.setUpdateType("UPDATE");
+                break;
+        }
+    }
+
+    public String getStockName() {
+        return stockName;
+    }
+
+    public void setStockName(String stockName) {
+        this.stockName = stockName;
+    }
+
+    public Date getTime() {
+        return time;
+    }
+
+    public void setTime(Date time) {
+        this.time = time;
+    }
+
+    public String getOrderID() {
+        return orderID;
+    }
+
+    public void setOrderID(String orderID) {
+        this.orderID = orderID;
+    }
+
+    public int getNumberOfShares() {
+        return numberOfShares;
+    }
+
+    public void setNumberOfShares(int numberOfShares) {
+        this.numberOfShares = numberOfShares;
+    }
+
+    public String getUpdateType() {
+        return updateType;
+    }
+
+    public void setUpdateType(String updateType) {
+        this.updateType = updateType;
+    }
+
 }
