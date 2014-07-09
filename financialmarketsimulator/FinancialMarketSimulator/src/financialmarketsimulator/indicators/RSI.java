f604740494277ec614bb05ff1fe16cf69c5252a3
MadimetjaShika <mmjshika@gmail.com>
2014-07-09 15:46:45 +0200

    Update on RSI

--- financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/indicators/RSI.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/indicators/RSI.java
@@ -1,4 +1,3 @@
-
 package financialmarketsimulator.indicators;
 
 /**
@@ -6,5 +5,32 @@ package financialmarketsimulator.indicators;
  * @brief Relative Strength Index
  */
 public class RSI {
-    
+
+    /**
+     * Variable housing RSI value
+     */
+    private double RSIValue;
+    private double relativeStrength;
+
+    public RSI() {
+    }
+
+    /**
+     * @brief Calculates and returns the RSI value
+     * @return Double value representing the RSI value
+     */
+    public double calculateRSI() {
+        RSIValue = 100 - (100 / (1 + calculateRS()));
+        return RSIValue;
+    }
+
+    /**
+     * @brief Calculates and returns the Relative Strength over 14 days
+     * @return Double value representing the relative strength value.
+     * @todo Calculate Relative Strength.
+     */
+    public double calculateRS() {
+        relativeStrength = 0;
+        return relativeStrength;
+    }
 }
