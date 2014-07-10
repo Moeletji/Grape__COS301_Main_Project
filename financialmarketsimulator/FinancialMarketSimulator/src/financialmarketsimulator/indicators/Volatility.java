619c1cbc910634e23142aad6b2d8d70bca35e6f9
Moeletji <mdsemenya@gmail.com>
2014-07-10 21:51:34 +0200

    Implemented Volatility class

--- financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/indicators/Volatility.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/indicators/Volatility.java
@@ -1,5 +1,11 @@
-// Error reading included file Templates/Classes/Templates/Licenses/license-Financial Market Simulator Licence.txt
 package financialmarketsimulator.indicators;
+
+//Error reading included file Templates/Classes/Templates/Licenses/license-Financial Market Simulator Licence.txt
+
+import financialmarketsimulator.indicators.SMA;
+import financialmarketsimulator.market.MarketEntryAttemptBook;
+import financialmarketsimulator.marketData.MatchedMarketEntryAttempt;
+import java.util.Vector;
 
 /**
  *
@@ -9,4 +15,47 @@ package financialmarketsimulator.indicators;
 
 public class Volatility {
     
+    private int period;
+    private SMA sma;
+    private double mean;
+    private MarketEntryAttemptBook data;
+    private double sd;
+    
+    public Volatility(int _period,MarketEntryAttemptBook _data )
+    {
+        this.period = _period;
+        sma = new SMA(_period);
+        this.data = _data;
+        
+    }
+          
+    public double getMean()
+    {
+        //edit SMA class
+        mean = sma.calculateSMA();
+        return mean;
+    }
+    
+    public double calculateSD()
+    {
+        if (period <=0 || data.isEmpty() || data.getMatchedOrders().size()<period+1)
+            return 0.0;
+        
+        int range = data.getMatchedOrders().size()-1-period;
+        int length = data.getMatchedOrders().size()-1;
+        double variance = 0;
+        Vector<MatchedMarketEntryAttempt> temp = data.getMatchedOrders();
+        
+        for (int i=range;i<length-1;i++)
+        {
+            variance += ((temp.get(i).getPrice() - getMean())*(temp.get(i).getPrice() - getMean()));
+        }
+        sd = Math.sqrt(variance/period);
+        return sd;
+    }
+    
+    public double getSD()
+    {
+        return sd;
+    }
 }
