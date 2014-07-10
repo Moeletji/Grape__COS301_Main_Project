015f78aff4aa4c6f989779a80f0201ad4881ba43
MadimetjaShika <mmjshika@gmail.com>
2014-07-10 13:33:04 +0200

    Chart Patterns

---
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/chartPatterns/elliotWave.java
@@ -0,0 +1,13 @@
+package financialmarketsimulator.chartPatterns;
+
+import com.tictactec.ta.lib.Core;
+import com.tictactec.ta.lib.MInteger;
+import com.tictactec.ta.lib.RetCode;
+
+/**
+ *
+ * @brief
+ */
+public class elliotWave {
+    
+}
