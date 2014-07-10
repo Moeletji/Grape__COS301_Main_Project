19218b0427527c96d346ab001f4955dd6203a25e
Moeletji <mdsemenya@gmail.com>
2014-07-10 08:39:35 +0200

    Added new classes

---
+++ financialmarketsimulator/FinancialMarketSimulator/test/strategiesUnitTests/BollingerBandsUnitTest.java
@@ -0,0 +1,12 @@
+// Error reading included file Templates/Classes/Templates/Licenses/license-Financial Market Simulator Licence.txt
+package strategiesUnitTests;
+
+/**
+ *
+ * @author Moeletji
+ */
+
+
+public class BollingerBandsUnitTest {
+    
+}
