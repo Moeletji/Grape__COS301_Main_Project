8c96157c6c39cb4ce9f95c376eec9900ba4a8e3b
Moeletji <mdsemenya@gmail.com>
2014-07-12 11:19:28 +0200

    Added Exception class

---
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/exception/NotEnoughDataException.java
@@ -0,0 +1,16 @@
+//Error reading included file Templates/Classes/Templates/Licenses/license-Financial Market Simulator Licence.txt
+package financialmarketsimulator.exception;
+
+/**
+ *
+ * @author Moeletji
+ */
+
+
+public class NotEnoughDataException extends Exception {
+    
+    public NotEnoughDataException()
+    {
+        super("Not Enough Data Exception");
+    }
+}
