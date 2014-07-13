05d719d8005668d27288be485dd620b2f826124f
MadimetjaShika <mmjshika@gmail.com>
2014-07-13 18:07:39 +0200

    Example of how to use JFreeCharts
    Also uploaded the test database used in the database folder

--- financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/chartPatterns/example.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/chartPatterns/example.java
@@ -1,34 +1,106 @@
-
 package financialmarketsimulator.chartPatterns;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Random;
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartFrame;
 import org.jfree.chart.JFreeChart;
-import org.jfree.data.general.DefaultPieDataset;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.jdbc.JDBCCategoryDataset;
 
 /**
  *
- * @brief Example class of how to use JFreeCharts, the charts library 
+ * @brief Example class of how to use JFreeCharts, the charts library. The chart is
+ * generated from dummy data created in the test_table table from the test_database 
+ * database. Just an example of how to use the JFreeCharts.
  */
 public class example {
 
+    private Connection con;
+    private Statement st;
+    private ResultSet rs;
+    private final String tableName = "test_table";
+    private final String databaseName = "test_db";
+    private final String user = "root";
+    private final String pass = "";
+    private double[][] values;
+    private JDBCCategoryDataset dataset;
+
+    public void example() {
+    }
+
+    public void connectToDB() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/" + databaseName, user, pass);
+            st = con.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @brief USED FOR POPULATING RANDOM DATA INTO THE test_table TABLE!
+     */
+    public void populate() {
+        double amount;
+        double range = 100;
+        Random random = new Random();
+
+        try {
+
+            for (int i = 1; i < 31; i++) {
+                double scaled = random.nextDouble() * range;
+                amount = scaled + 0;
+
+                String query1 = "INSERT INTO " + tableName + " (Date,Price) VALUES(?,?)";
+                PreparedStatement ps = con.prepareStatement(query1);
+                ps.setString(1, "07-" + i);
+                ps.setDouble(2, amount);
+                ps.execute();
+            }
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void execute() {
+        try {
+            String query1 = "SELECT * FROM " + tableName;
+            rs = st.executeQuery(query1);
+
+            dataset = new JDBCCategoryDataset(con, query1);
+
+            int i = 1;
+            while (rs.next()) {
+                System.out.println(i + ".  " + rs.getString("Date") + "   : " + rs.getString("Price"));
+                i++;
+            }
+
+            System.out.println("END.");
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
     public static void main(String[] args) {
-        // create a dataset...
-        DefaultPieDataset data = new DefaultPieDataset();
-        data.setValue("Category 1", 43.2);
-        data.setValue("Category 2", 27.9);
-        data.setValue("Category 3", 79.5);
-        // create a chart...
-        JFreeChart chart = ChartFactory.createPieChart(
-                "Sample Pie Chart",
-                data,
-                true, // legend?
-                true, // tooltips?
-                false // URLs?
-        );
-        // create and display a frame...
-        ChartFrame frame = new ChartFrame("First", chart);
-        frame.pack();
+
+        example exam = new example();
+        exam.connectToDB();
+        //exam.populate(); //WAS USED FOR POPULATING DATA INTO THE DATABASE
+        exam.execute();
+
+        //Creating the chart
+        JFreeChart chart = ChartFactory.createLineChart("Basic Line Chart", "Date", "Price", exam.dataset, PlotOrientation.VERTICAL, false, true, true);
+        ChartFrame frame = new ChartFrame("Basic Line Chart", chart);
         frame.setVisible(true);
+        frame.setSize(1000, 650);
     }
 }
