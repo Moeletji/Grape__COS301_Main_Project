dae22457dd74208c9ceecc8c5f95f8819260cddc
MadimetjaShika <mmjshika@gmail.com>
2014-07-17 17:49:09 +0200

    Crossover and Filter Unit Test classes

---
+++ financialmarketsimulator/FinancialMarketSimulator/test/strategyUnitTests/CrossoverUnitTest.java
@@ -0,0 +1,38 @@
+
+package strategyUnitTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @brief Test Class for the Crossover Strategy Class
+ */
+public class CrossoverUnitTest {
+    
+    public CrossoverUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+    
+    
+}
---
+++ financialmarketsimulator/FinancialMarketSimulator/test/strategyUnitTests/FilterUnitTest.java
@@ -0,0 +1,36 @@
+
+package strategyUnitTests;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @brief Test class for the Filter Strategy Class
+ */
+public class FilterUnitTest {
+    
+    public FilterUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+    
+}
