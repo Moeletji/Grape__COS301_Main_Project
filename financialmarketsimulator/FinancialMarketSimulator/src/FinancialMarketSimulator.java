594d0c4d43822db87d31394dc99261a987f2b78e
danmakk <danmakk@gmail.com>
2014-07-18 16:16:04 +0200

    Started with simulation

--- financialmarketsimulator/FinancialMarketSimulator/src/FinancialMarketSimulator.java
+++ financialmarketsimulator/FinancialMarketSimulator/src/FinancialMarketSimulator.java
@@ -1,5 +1,7 @@
 
+import financialmarketsimulator.market.MarketExchange;
 import financialmarketsimulator.market.StockManager;
+import java.util.ArrayList;
 
 /**
  * @brief Main project execution class
@@ -8,7 +10,23 @@ import financialmarketsimulator.market.StockManager;
 public class FinancialMarketSimulator {
 
     public static void main(String[] args) {
-        StockManager manager = new StockManager();
+        
+        MarketExchange exchange = MarketExchange.getInstance("JSE");
+        
+        ArrayList<StockManager> managers =  new ArrayList();
+        
+        String [] names = {"INV", "IPSA", "LBH", "ABSAB", "AIP", "ABL", "HWN", "ILV"};
+        
+        int size = 8;
+        
+        for(int i = 0; i < size; i++){
+             exchange.addStockManager(new StockManager(names[i]));
+        }
+        
+        for(StockManager manager : managers){
+            exchange.addStockManager(manager);
+        }
+        
     }
 
 }
