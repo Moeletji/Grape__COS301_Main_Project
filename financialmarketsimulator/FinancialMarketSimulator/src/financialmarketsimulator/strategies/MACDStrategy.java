1d1ac6cc865f3e7cf3dba4655409cb2f2d6ef1a9
Moeletji <mdsemenya@gmail.com>
2014-07-28 07:58:29 +0200

    Added new strategy

---
+++ financialmarketsimulator/FinancialMarketSimulator/src/financialmarketsimulator/strategies/MACDStrategy.java
@@ -0,0 +1,12 @@
+//Error reading included file Templates/Classes/Templates/Licenses/license-Financial Market Simulator Licence.txt
+package financialmarketsimulator.strategies;
+
+/**
+ *
+ * @author Grape <cos301.mainproject.grape@gmail.com>
+ */
+
+
+public class MACDStrategy {
+    
+}
